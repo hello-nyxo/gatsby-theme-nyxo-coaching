5cfe4c5bf5e0ca21fc40153f7df3b70c0aa1c730
Perttu Lähteenlahti <perttu.lahteenlahti@qvik.fi>
2020-10-25 19:11:49 +0200

    Fix path

--- .github/scripts/decrypt-team-provider.sh
+++ .github/scripts/decrypt-team-provider.sh
@@ -3,4 +3,4 @@
 # Decrypt the file
 mkdir $HOME/
 gpg --quiet --batch --yes --decrypt --passphrase="$PASS_PHRASE" \
---output ./site/amplify/team-provider-info.json site/team/team-provider-info.json.gpg
+--output ./site/amplify/team-provider-info.json team/team-provider-info.json.gpg

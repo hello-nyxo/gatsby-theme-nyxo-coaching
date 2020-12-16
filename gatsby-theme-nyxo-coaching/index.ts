2a8512f7695e3719c3547a58252026a1a62e6e57
Perttu Lähteenlahti <perttu.lahteenlahti@qvik.fi>
2020-12-17 00:15:11 +0200

    Add Themes

--- gatsby-theme-nyxo-coaching/index.ts
+++ gatsby-theme-nyxo-coaching/index.ts
@@ -1,3 +1,4 @@
 export { RecentlyUpdated } from "@components/personalization/RecentlyUpdated"
 export { SuggestedContent } from "@components/personalization/SuggestedContent"
 export { GlobalSearch as Search } from "@components/search/Search"
+export { lightTheme, darkTheme } from "@styles/themes"
